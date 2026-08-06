3e7083391306ff97ade5da9c7a4f6bbff54691b4
NaveenKumar <project2017project@gmail.com>
2026-08-06 14:31:32 +0530

    mail sent funftion

--- app/User.php
+++ app/User.php
@@ -6,6 +6,10 @@ use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\URL;
 
 class User extends Authenticatable
 {
@@ -37,4 +41,45 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function sendCustomVerificationEmail()
+    {
+        $mail = DB::table('mail_settings')->where('slug', 'password')->first();
+
+        if ($mail) {
+            Config::set('mail', [
+                'driver'     => $mail->driver,
+                'host'       => $mail->host,
+                'port'       => $mail->port,
+                'from'       => ['address' => $mail->from_address, 'name' => $mail->from_name],
+                'encryption' => $mail->encryption,
+                'username'   => $mail->username,
+                'password'   => $mail->password,
+                'sendmail'   => '/usr/sbin/sendmail -bs',
+                'pretend'    => false,
+            ]);
+        }
+
+        $url = URL::temporarySignedRoute(
+            'verification.verify',
+            now()->addMinutes(60),
+            ['id' => $this->id, 'hash' => sha1($this->email)]
+        );
+
+        $messageBody = "<!DOCTYPE html><html><body>
+            <p>Please verify your email: <a href='{$url}'>Verify Email</a></p>
+            <p>This link expires in 60 minutes.</p>
+            </body></html>";
+
+        $data = [
+            'msg'     => $messageBody,
+            'subject' => 'Verify your The Zouple account email',
+            'email'   => $this->email,
+        ];
+
+        Mail::send([], [], function ($message) use ($data) {
+            $message->to($data['email'])->subject($data['subject'])
+                ->setBody($data['msg'], 'text/html');
+        });
+    }
 }
